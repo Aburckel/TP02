449398bd424380395052f3514ecf2a1a6fdb4fb3
Anthony BURCKEL <anthonybu@free.fr>
2019-10-13 15:51:31 +0200

    first commit

---
+++ TP02/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TetiereComponent } from './tetiere/tetiere.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TetiereComponent,
+    FooterComponent,
+    FormulaireComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
